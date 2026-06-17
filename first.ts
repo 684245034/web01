1a64ff2aa468d619cd1734a24e5b43574cbeb23b
684245034 <684245034@webmail.npru.ac.th>
2026-06-17 11:15:53 +0700

    ส่งงานทำในห้อง

---
+++ first.ts
@@ -0,0 +1,22 @@
+const fname: string = "Chakrit";
+const lname: string = "Sueaklei";
+const age: number = 20;
+const dreamJob: string = "Software Engineer";
+const course: string = "Object Oriented Programming";
+const mark: number = 80;
+const passed: boolean = true;
+
+console.log(`Name: ${fname} ${lname}`);
+console.log(`Age: ${age}`);
+console.log(`Dream Job: ${dreamJob}`);
+console.log(`Course: ${course}`);
+console.log(`Mark: ${mark}`);
+console.log(`Pass: ${passed}`);
+
+const courses: string[] = ["Object Oriented Programming", "Data Structure", "Database"];
+const marks: number[] = [87, 72, 81];
+console.log(marks);
+console.log(courses[0], marks[0]);
+console.log(courses[1], marks[1]);
+console.log(courses[2], marks[2]);
+
